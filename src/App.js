519e5abfec4817337ab7495882d2cb6f22cb564c
Kristjan Eimre <kristjaneimre@gmail.com>
2023-01-25 23:06:29 +0200

    Add Materials Cloud Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import SSSP from "./components/SSSP";
 
+import MaterialsCloudHeader from "react-materialscloud-header";
+
 function App() {
   return (
-    <div className="App">
-      <SSSP />
-    </div>
+    <MaterialsCloudHeader
+      activeSection={"discover"}
+      breadcrumbsPath={[
+        { name: "Discover", link: "https://www.materialscloud.org/discover" },
+        { name: "SSSP", link: null },
+      ]}
+    >
+      <div className="App">
+        <SSSP />
+      </div>
+    </MaterialsCloudHeader>
   );
 }
 
