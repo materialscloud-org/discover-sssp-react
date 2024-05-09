cd78961374535630ddfb373b6c296167e94235c0
Edan Bainglass <edan.bainglass@gmail.com>
2024-05-09 17:13:13 +0200

    Replaces classes with functions

--- src/components/PTable.js
+++ src/components/PTable.js
@@ -4,101 +4,73 @@ import { element_symbols } from "./ptable_symbols";
 
 import "./PTable.css";
 
-class Element extends React.Component {
-  constructor(props) {
-    super(props);
+const Element = ({ number, symbol, color, elemInfo, linkBase }) => {
+  const link = `${linkBase}/${symbol}`;
+  const disabled = elemInfo == null;
 
-    this.link = `${this.props.link_base}/${this.props.symbol}`;
-
-    this.disabled = false;
-    if (this.props.elem_info == null) {
-      this.disabled = true;
-    }
-
-    this.state = {};
+  let eClass = `element element-${number}`;
+  if (number >= 58 && number <= 71) {
+    eClass += " lanthanide";
   }
 
-  render() {
-    let e_class = `element element-${this.props.num}`;
-
-    if (this.props.num >= 58 && this.props.num <= 71) {
-      e_class += " lanthanide";
-    }
-
-    let cutoff_text = null;
-    if (this.disabled) {
-      // the css class will disable the link
-      e_class += " element-disabled";
-    } else {
-      let wfc_cutoff = this.props.elem_info["cutoff"];
-      let rho_cutoff = this.props.elem_info["rho_cutoff"];
-      cutoff_text = (
-        <div className="elem_num">
-          {wfc_cutoff}
-          <sub>({rho_cutoff})</sub>
-        </div>
-      );
-    }
-
-    return (
-      <a
-        className={e_class}
-        style={{ background: this.props.color }}
-        href={this.link}
-      >
-        <div className="elem_sym">{this.props.symbol}</div>
-        {cutoff_text}
-      </a>
+  let cutoffText = null;
+  if (!disabled) {
+    const { cutoff: wfcCutoff, rho_cutoff: rhoCutoff } = elemInfo;
+    cutoffText = (
+      <div className="elem_num">
+        {wfcCutoff}
+        <sub>({rhoCutoff})</sub>
+      </div>
     );
   }
-}
 
-class PTable extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  return (
+    <a
+      className={`${eClass}${disabled ? " element-disabled" : ""}`}
+      style={{ background: color }}
+      href={link}
+    >
+      <div className="elem_sym">{symbol}</div>
+      {cutoffText}
+    </a>
+  );
+};
 
-  makeElements = (start, end) => {
-    let items = [];
+const PTable = ({ ssspData, pseudoMetadata, linkBase }) => {
+  const makeElements = (start, end) => {
+    const items = [];
     for (let i = start; i <= end; i++) {
-      let symbol = element_symbols[i];
-
-      // if this returns undefined, the element will be disabled later on
-      let elem_info = this.props.sssp_data[symbol];
-
-      let color = "#dddddd"; // color of disabled elements
-      if (elem_info) {
-        let pp = elem_info["pseudopotential"];
-        color = this.props.pseudo_metadata[pp]["background_color"];
-      }
+      const symbol = element_symbols[i];
+      const elemInfo = ssspData[symbol];
+      const color = elemInfo
+        ? pseudoMetadata[elemInfo.pseudopotential].background_color
+        : "#dddddd";
 
       items.push(
         <Element
           key={i}
-          num={i}
+          number={i}
           symbol={symbol}
           color={color}
-          elem_info={elem_info}
-          link_base={this.props.link_base}
+          elemInfo={elemInfo}
+          linkBase={linkBase}
         />
       );
     }
     return items;
   };
 
-  render() {
-    return (
-      <div className="ptable_outer">
-        <div className="ptable">
-          {this.makeElements(1, 57)}
-          {this.makeElements(72, 89)}
-          {this.makeElements(104, 118)}
-          {this.makeElements(58, 71)}
-          {this.makeElements(90, 103)}
-        </div>
+  return (
+    <div className="ptable_outer">
+      <div className="ptable">
+        {makeElements(1, 57)}
+        {makeElements(72, 89)}
+        {makeElements(104, 118)}
+        {makeElements(58, 71)}
+        {makeElements(90, 103)}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default PTable;
--- src/components/SSSP.js
+++ src/components/SSSP.js
@@ -5,23 +5,17 @@ import PTable from "./PTable";
 import pseudo_metadata from "../data/metadata.json";
 import sssp_efficiency from "../data/sssp_efficiency.json";
 
-class SSSP extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <div>Placeholder for information, legend, etc</div>
-        <PTable
-          sssp_data={sssp_efficiency}
-          pseudo_metadata={pseudo_metadata}
-          link_base="https://www.materialscloud.org/discover/sssp/plot/efficiency"
-        />
-      </div>
-    );
-  }
-}
+const SSSP = () => {
+  return (
+    <div>
+      <div>Placeholder for information, legend, etc</div>
+      <PTable
+        ssspData={sssp_efficiency}
+        pseudoMetadata={pseudo_metadata}
+        linkBase="https://www.materialscloud.org/discover/sssp/plot/efficiency"
+      />
+    </div>
+  );
+};
 
 export default SSSP;
