35914ce327bf7adfca4fe5cb4306c15dbcb73b6c
Edan Bainglass <edan.bainglass@gmail.com>
2024-05-14 14:18:15 +0200

    Add instructions placeholder

--- src/pages/pseudos/index.tsx
+++ src/pages/pseudos/index.tsx
@@ -3,12 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import DetailsPage from "./details";
 import TablePage from "./table";
 
+import "./index.css";
+
 const PseudosPage = () => {
   return (
-    <Routes>
-      <Route path="" element={<TablePage />} />
-      <Route path=":element" element={<DetailsPage />} />
-    </Routes>
+    <div className="pseudos">
+      <div className="instructions">Instructions placeholder</div>
+      <Routes>
+        <Route path="" element={<TablePage />} />
+        <Route path=":element" element={<DetailsPage />} />
+      </Routes>
+    </div>
   );
 };
 
