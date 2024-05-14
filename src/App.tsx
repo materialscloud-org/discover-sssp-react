8eaa409034f8412c8eb90c37e2efa0b8d4135b90
Edan Bainglass <edan.bainglass@gmail.com>
2024-05-14 14:14:11 +0200

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Tab, Tabs } from "react-bootstrap";
+import { Card, Tab, Tabs } from "react-bootstrap";
 import {
   Navigate,
   Route,
@@ -36,29 +36,43 @@ function App() {
     >
       <div className="App">
         <div className="main-page">
-          <Header
-            title="Standard solid-state pseudopotentials (SSSP)"
-            subtitle="A standard solid-state pseudopotentials (SSSP) library optimized for precision or efficiency."
-            doi_ids={["f3-ym"]}
-            logo={logo}
-          />
-          <Router>
-            <RoutedTabs />
-            <Routes>
-              <Route path="discover/sssp">
-                <Route path="efficiency/*" element={<PseudosPage />} />
-                <Route path="precision/*" element={<PseudosPage />} />
-                <Route path="about" element={<AboutPage />} />
-                <Route path="" element={<Navigate replace to="efficiency" />} />
-              </Route>
-            </Routes>
-          </Router>
+          <Card>
+            <Card.Body className="py-2">
+              <Header
+                title="Standard solid-state pseudopotentials (SSSP)"
+                subtitle="A standard solid-state pseudopotentials (SSSP) library optimized for precision or efficiency."
+                doi_ids={["f3-ym"]}
+                logo={logo}
+              />
+            </Card.Body>
+          </Card>
+          <Card className="mt-3">
+            <Router>
+              <Card.Header className="tab-controls">
+                <RoutedTabs />
+              </Card.Header>
+              <Card.Body id="main-card" className="p-4">
+                <Routes>
+                  <Route path="discover/sssp">
+                    <Route path="efficiency/*" element={<PseudosPage />} />
+                    <Route path="precision/*" element={<PseudosPage />} />
+                    <Route path="about" element={<AboutPage />} />
+                    <Route
+                      path=""
+                      element={<Navigate replace to="efficiency" />}
+                    />
+                  </Route>
+                </Routes>
+              </Card.Body>
+            </Router>
+          </Card>
         </div>
       </div>
     </MaterialsCloudHeader>
   );
 }
 
+// TODO generalize and refactor
 const RoutedTabs = () => {
   const [active, setActive] = useState("efficiency");
   const location = useLocation();
--- src/pages/pseudos/index.tsx
+++ src/pages/pseudos/index.tsx
@@ -5,13 +5,10 @@ import TablePage from "./table";
 
 const PseudosPage = () => {
   return (
-    <div className="body">
-      {/* <div className="description"></div> */}
-      <Routes>
-        <Route path="" element={<TablePage />} />
-        <Route path=":element" element={<DetailsPage />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="" element={<TablePage />} />
+      <Route path=":element" element={<DetailsPage />} />
+    </Routes>
   );
 };
 
