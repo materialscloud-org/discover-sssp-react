e39f8e494b59a8ba264f15f08b895c1a71a44714
Edan Bainglass <edan.bainglass@gmail.com>
2024-09-03 16:22:11 +0200

    Move accuracy toggle buttons under legend

--- src/SSSPTable/pages/pseudos/table/TablePage.models.ts
+++ src/SSSPTable/pages/pseudos/table/TablePage.models.ts
@@ -1,3 +1,8 @@
+export interface AccuracyToggleProps {
+  accuracies: string[];
+  activeAccuracy: string;
+}
+
 export default interface TablePageProps {
   accuracies: string[];
 }
--- src/SSSPTable/pages/pseudos/table/TablePage.tsx
+++ src/SSSPTable/pages/pseudos/table/TablePage.tsx
@@ -11,12 +11,11 @@ import pseudoMetadata from "@sssp/data/metadata.json";
 import ssspEfficiency from "@sssp/data/sssp_efficiency.json";
 import ssspPrecision from "@sssp/data/sssp_precision.json";
 
-import TablePageProps from "./TablePage.models";
+import TablePageProps, { AccuracyToggleProps } from "./TablePage.models";
 import styles from "./TablePage.module.scss";
 
 const TablePage: React.FC<TablePageProps> = ({ accuracies }) => {
   // TODO use a service to fetch the data
-  const navigate = useNavigate();
   const location = useLocation();
   const { activeAccuracy, setActiveAccuracy } = useContext(AccuracyContext);
 
@@ -30,27 +29,40 @@ const TablePage: React.FC<TablePageProps> = ({ accuracies }) => {
 
   return (
     <div id="table-page">
-      <ToggleButtonGroup
-        id={styles["accuracy-controls"]}
-        type="radio"
-        name="accuracy"
-        value={activeAccuracy}
-        onChange={(value) => navigate(`../${value}`)}
-      >
-        {accuracies.map((accuracy) => (
-          <ToggleButton key={accuracy} id={accuracy} value={accuracy}>
-            {capitalize(accuracy)}
-          </ToggleButton>
-        ))}
-      </ToggleButtonGroup>
       <div className="sssp-pseudos-header">
         SSSP {activeAccuracy} (v{ssspVersion})
       </div>
       <HoverProvider>
         <PseudosLegend pseudoMetadata={pseudoMetadata} />
+        <AccuracyToggle
+          accuracies={accuracies}
+          activeAccuracy={activeAccuracy}
+        />
         <PeriodicTable pseudoMetadata={pseudoMetadata} ssspData={ssspData} />
       </HoverProvider>
     </div>
+  );
+};
+
+const AccuracyToggle: React.FC<AccuracyToggleProps> = ({
+  accuracies,
+  activeAccuracy,
+}) => {
+  const navigate = useNavigate();
+  return (
+    <ToggleButtonGroup
+      id={styles["accuracy-controls"]}
+      type="radio"
+      name="accuracy"
+      value={activeAccuracy}
+      onChange={(value) => navigate(`../${value}`)}
+    >
+      {accuracies.map((accuracy) => (
+        <ToggleButton key={accuracy} id={accuracy} value={accuracy}>
+          {capitalize(accuracy)}
+        </ToggleButton>
+      ))}
+    </ToggleButtonGroup>
   );
 };
 
