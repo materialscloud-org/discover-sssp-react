d03be581f7d0077899d0b2d8e6074aa95e509c55
Edan Bainglass <edan.bainglass@gmail.com>
2024-09-30 08:10:19 +0200

    Add plotly dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    nodePolyfills({
+      protocolImports: true,
+    }),
+  ],
   server: {
     port: 3000,
   },
