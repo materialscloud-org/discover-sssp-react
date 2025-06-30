5a99d9c8d5dc5c8c1f3eaab186a10fc79b871a16
Edan Bainglass <edan.bainglass@gmail.com>
2025-06-30 09:06:23 +0200

    Refactor overview plot

---
+++ src/SsspApp/plotting/OverviewPlots/OverviewPlot.models.ts
@@ -0,0 +1,39 @@
+interface OverviewPlotProps {
+  element: string;
+  accuracy: string;
+  convergence: string;
+}
+
+interface PseudoResponse {
+  conff: string;
+  pseudos: Pseudo[];
+}
+
+interface PseudoQuantity {
+  cutoffs: number[];
+  values: number[];
+}
+
+interface Pseudo {
+  conff: string;
+  name: string;
+  color: string;
+  Z: number;
+  quantities: {
+    metadata?: {
+      avg_nu: number;
+      max_nu: number;
+      ang_nu: number;
+      max_conf: string;
+    };
+    phonon_frequencies?: PseudoQuantity & { error: number[]; ref: number };
+    pressure?: PseudoQuantity;
+    cohesive_energy?: PseudoQuantity & { ref: number };
+    eos?: PseudoQuantity;
+    bands?: {
+      cutoffs: number[];
+      eta_c: number[];
+      max_diff_c: number[];
+    };
+  };
+}
