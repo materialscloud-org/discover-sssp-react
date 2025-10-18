28908a3a903a4c679d5cbfa61496cc08161a60d0
Edan Bainglass <edan.bainglass@gmail.com>
2025-10-18 10:14:42 +0200

    Fix `DATA_URL`

--- src/SsspApp/common/config.ts
+++ src/SsspApp/common/config.ts
@@ -3,4 +3,4 @@ export const BASE_URL = import.meta.env.BASE_URL || "/";
 
 // raw data location on github
 export const DATA_URL =
-  "https://raw.githubusercontent.com/materialscloud-org/discover-sssp-react/refs/heads/main/public/data/";
+  "https://raw.githubusercontent.com/materialscloud-org/discover-sssp-react/refs/heads/main/public/data";
