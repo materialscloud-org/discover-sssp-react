211335e9f9bca5108e1dd1cd0ef4ab9fd2940724
Edan Bainglass <edan.bainglass@gmail.com>
2025-11-01 12:37:58 +0100

    Move `AboutPage` to single file

--- src/SsspApp/pages/index.ts
+++ src/SsspApp/pages/index.ts
@@ -1,3 +1,3 @@
+export { default as AboutPage } from "./AboutPage";
 export { default as InvalidPage } from "./InvalidPage";
 export { default as PseudosPage } from "./pseudos";
-export { default as AboutPage } from "./about";
