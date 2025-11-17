65045e5cb7d564f0535d66aef708cf85ca13a0ec
Edan Bainglass <edan.bainglass@gmail.com>
2025-11-17 14:55:35 +0100

    Updates

---
+++ assets/NoDataMessage-CwK9x58P.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DjrfjVru.js";const s=()=>t.jsx("div",{style:{textAlign:"center"},children:t.jsx("strong",{children:"No data available"})});export{s as N};
--- assets/NoDataMessage-DU2QyKrj.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BXRE74-o.js";const s=()=>t.jsx("div",{style:{textAlign:"center"},children:t.jsx("strong",{children:"No data available"})});export{s as N};
