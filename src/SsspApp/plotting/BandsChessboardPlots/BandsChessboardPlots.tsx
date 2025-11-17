762baf6a288162f6b7d70028a343980b2a03afbc
Edan Bainglass <edan.bainglass@gmail.com>
2025-11-17 12:58:40 +0100

    Update bands chessboard plot formatting

--- src/SsspApp/plotting/BandsChessboardPlots/BandsChessboardPlots.tsx
+++ src/SsspApp/plotting/BandsChessboardPlots/BandsChessboardPlots.tsx
@@ -1,5 +1,5 @@
 import { useContext, useMemo } from "react";
-import { Col } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 
 import { LoadingSpinner } from "@sssp/components";
 import { PseudosContext } from "@sssp/context";
@@ -46,24 +46,26 @@ const BandsChessboardPlots: React.FC<BandsChessboardPlotsProps> = ({
         demonstration purposes only.
       </div>
       <div id={styles["chessboard-plots"]}>
-        <Col xl={12} xxl={6}>
-          <BandsChessboardPlot
-            pseudos={pseudos}
-            values={eta_v}
-            title="V"
-            colorMax={Math.max(...eta_v.flat())}
-            tileClickHandler={tileClickHandler}
-          />
-        </Col>
-        <Col xl={12} xxl={6}>
-          <BandsChessboardPlot
-            pseudos={pseudos}
-            values={eta_10}
-            title="10"
-            colorMax={Math.max(...eta_10.flat())}
-            tileClickHandler={tileClickHandler}
-          />
-        </Col>
+        <Row className="justify-content-center g-0">
+          <Col xxl="6">
+            <BandsChessboardPlot
+              pseudos={pseudos}
+              values={eta_v}
+              title="V"
+              colorMax={Math.max(...eta_v.flat())}
+              tileClickHandler={tileClickHandler}
+            />
+          </Col>
+          <Col xxl="6">
+            <BandsChessboardPlot
+              pseudos={pseudos}
+              values={eta_10}
+              title="10"
+              colorMax={Math.max(...eta_10.flat())}
+              tileClickHandler={tileClickHandler}
+            />
+          </Col>
+        </Row>
       </div>
     </div>
   );
