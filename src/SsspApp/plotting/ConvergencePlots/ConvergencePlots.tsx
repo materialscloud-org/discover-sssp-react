59b8f369308c3bb548d9992b00dbb8013c8f0b02
Edan Bainglass <edan.bainglass@gmail.com>
2025-11-18 14:14:38 +0100

    Move convergence plot description above the plot

--- src/SsspApp/plotting/ConvergencePlots/ConvergencePlots.tsx
+++ src/SsspApp/plotting/ConvergencePlots/ConvergencePlots.tsx
@@ -31,6 +31,40 @@ const ConvergencePlots: React.FC<ConvergencePlotsProps> = ({ element }) => {
     <LoadingSpinner />
   ) : (
     <div id="convergence-summary">
+      <div id={styles["convergence-plot-description"]}>
+        <h5>Convergence pattern plots according to the SSSP protocol</h5>
+        <p>
+          We report here the zone-boundary phonons, cohesive energy, pressure,
+          and band structure versus the wavefunction cutoff for all the
+          considered pseudopotential libraries.
+        </p>
+        <p>
+          The colored horizontal dashed lines correspond to the thresholds of
+          the SSSP efficiency criteria, whereas the gray dotted lines correspond
+          to the precision criteria. The recommended efficiency (precision)
+          pseudopotential is marked with a colored{" "}
+          <span style={{ color: "green" }}>square</span> (
+          <span style={{ color: "red" }}>circle</span>) at the recommended
+          cutoff.
+        </p>
+        On the left, we report:
+        <ul>
+          <li>the number of valence electrons of the pseudopotential</li>
+          <li>
+            the delta-factor with respect to the reference all-electron results
+          </li>
+          <li>the maximum phonon frequency</li>
+        </ul>
+        <p>
+          All calculations are performed on the ground-state elemental solids
+          unless stated otherwise (i.e. rare-earths and flourine). (see{" "}
+          <Link to="/about" style={{ textDecoration: "none" }}>
+            About SSSP
+          </Link>{" "}
+          for more details).
+        </p>
+      </div>
+      <hr />
       <CategorySelector
         categories={categories}
         activeCategories={activeCategories}
@@ -38,23 +72,6 @@ const ConvergencePlots: React.FC<ConvergencePlotsProps> = ({ element }) => {
       />
       <div id={styles["convergence-plot-container"]}>
         <ConvergencePlot element={element} pseudosMetadata={pseudosMetadata} />
-      </div>
-      <hr />
-      <div id={styles["convergence-plot-description"]}>
-        Convergence pattern plots according to the SSSP protocol: zone-boundary
-        phonons, cohesive energy, pressure and band structure versus the
-        wavefunction cutoff for all the considered pseudopotential libraries
-        (the colored horizontal dashed lines correspond to the thresholds of the
-        SSSP efficiency criteria, whereas the gray dotted lines correspond to
-        the precision criteria). The recommended efficiency (precision)
-        pseudopotential is marked with a dotted bounding square (circle) at the
-        recommended cutoff. On the right-hand side we report the number of
-        valence electrons of the pseudopotential, the delta-factor with respect
-        to the reference all-electron results and the maximum phonon frequency.
-        All calculations are performed on the ground-state elemental solids
-        unless stated otherwise (i.e. rare-earths and flourine). The circle
-        marks the pseudopotential and wavefunction cutoff chosen for the SSSP
-        library (see <Link to="/about">About SSSP</Link> for more details).
       </div>
     </div>
   );
