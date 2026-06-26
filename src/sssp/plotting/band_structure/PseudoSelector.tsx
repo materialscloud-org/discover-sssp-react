fb84249451980cb67566719c6ab93d7b7160e6af
Edan Bainglass <edan.bainglass@gmail.com>
2026-06-26 13:28:05 +0200

    Highlight SSSP pseudos in bands pseudo selectors

--- src/sssp/plotting/band_structure/PseudoSelector.tsx
+++ src/sssp/plotting/band_structure/PseudoSelector.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useContext, useMemo } from "react";
 import { Form } from "react-bootstrap";
 
 import { capitalize } from "@sssp/common/utils";
+import { ElementContext, PseudoContext } from "@sssp/context";
 
 interface PseudoSelectorProps {
   id?: string;
@@ -18,6 +19,35 @@ const PseudoSelector: React.FC<PseudoSelectorProps> = ({
   value,
   onSelect,
 }) => {
+  const { ssspPseudos } = useContext(ElementContext);
+  const { pseudosMetadata } = useContext(PseudoContext);
+
+  const [efficiencyPseudo, precisionPseudo] = useMemo(() => {
+    return [
+      [ssspPseudos?.efficiency?.library, ssspPseudos?.efficiency?.Z].join("-"),
+      [ssspPseudos?.precision?.library, ssspPseudos?.precision?.Z].join("-"),
+    ];
+  }, [ssspPseudos]);
+
+  const getStyle = (pseudo: string) => {
+    const isEfficiency = pseudo === efficiencyPseudo;
+    const isPrecision = pseudo === precisionPseudo;
+
+    const isSSSP = isEfficiency || isPrecision;
+
+    let [color, weight] = ["black", "normal"];
+    if (isSSSP) {
+      color =
+        pseudosMetadata[
+          isEfficiency
+            ? ssspPseudos.efficiency.library
+            : ssspPseudos.precision.library
+        ]?.color || "black";
+      weight = "bold";
+    }
+    return { color: color, fontWeight: weight };
+  };
+
   return (
     <Form.Group id={id}>
       <Form.Label htmlFor={`${which}-select`}>{capitalize(which)}:</Form.Label>
@@ -28,7 +58,7 @@ const PseudoSelector: React.FC<PseudoSelectorProps> = ({
         onChange={(e) => onSelect(e.target.value)}
       >
         {pseudos.map((pseudo) => (
-          <option key={pseudo} value={pseudo}>
+          <option key={pseudo} value={pseudo} style={getStyle(pseudo)}>
             {pseudo}
           </option>
         ))}
